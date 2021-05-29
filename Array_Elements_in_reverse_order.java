74e8c170e6e15c8fa42f9d2a437cb1727f8ac89c
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-29 14:43:39 +0530

    Create Array_Elements_in_reverse_order.java

---
+++ Array_Elements_in_reverse_order.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Printing_elements_of_array_in_reverse_order {
+    public static void main(String args[])
+    {
+        int [] array = new int[5];
+        array[0] = 20;
+        array[1] = 10;
+        array[2] = 11;
+        array[3] = 50;
+        array[4] = 34;
+
+        for (int i = array.length-1; i>=0; i-- )
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
+
