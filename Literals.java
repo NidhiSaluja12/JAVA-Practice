e75d3ba56160a3c650040dc3aedc2366cc91167a
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-02 11:20:05 +0530

    Create Literals.java

---
+++ Literals.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args)
+    {
+        byte age = 34; // byte - datatype ; age - variable ; 34 - literal
+        char ch = 'A'; // '' compulsary
+        float f1 = 5.6f;
+        short age1 = 34;
+        long age2 = 34232324;
+        double d1 = 4.22d; // d not neccessary
+        int age3 = 32;
+        boolean bool = true;
+
+
+        System.out.println(age);
+        String str = "Python";
+        System.out.println(str);
+
+
+    }
+}
